317c2df11d630d5db8f607174299ae396f3a48c0
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-12-09 09:24:51 +0530

    Added: validation for email regex

--- js/simple-form.js
+++ js/simple-form.js
@@ -7,9 +7,19 @@ const text = document.querySelector('#text');
             else textError.textContent = "Name is Incorrect ";
         });
 
+const email = document.querySelector('#email');
+    const emailError = document.querySelector('.email-error');
+    email.addEventListener('input', function() {
+        const regEmail = /^[A-Za-z0-9]+([._%+-][0-9a-zA-Z]+)*@[A-Za-z0-9]+([.]([a-zA-Z]{2,3})*)+$/;
+        if (regEmail.test(email.value))
+            emailError.textContent = "";
+        else emailError.textContent = "Email is Incorrect ";
+    });
+
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
 salary.addEventListener('input', function(){
     output.textContent = salary.value;
 });
+
